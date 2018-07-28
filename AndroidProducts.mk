30d9e278d50ee1eba94ad92ebc5b5e9e13772b6e
Dil3mm4 <dil3mm4.dev@gmail.com>
2018-07-28 12:12:47 +0000

    Bring up 32bit tables

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,5 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/table_arm64_a.mk \
-	$(LOCAL_DIR)/table_arm64_ab.mk
-
+	$(LOCAL_DIR)/table_arm64_ab.mk \
+	$(LOCAL_DIR)/table_arm_a.mk \
+	$(LOCAL_DIR)/table_arm_ab.mk
---
+++ table_arm_ab.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, device/phh/treble/base-pre.mk)
+include build/make/target/product/treble_common.mk
+$(call inherit-product, vendor/vndk/vndk-binder32.mk)
+$(call inherit-product, device/phh/treble/base.mk)
+
+$(call inherit-product, vendor/bootleggers/GSI/gsi32.mk)
+
+PRODUCT_NAME := table_arm_ab
+PRODUCT_DEVICE := table_arm_ab
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Table32 AB
+
+PRODUCT_PACKAGES += 
